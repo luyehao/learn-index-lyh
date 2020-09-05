ca048d1a57cc42714f6cfa7146ebd1c084995496
luyehao <47587450+luyehao@users.noreply.github.com>
2020-09-05 08:01:54 +0800

    Add files via upload

--- predict.py
+++ predict.py
@@ -5,6 +5,7 @@ from sklearn import datasets, linear_model
 from BTrees.OIBTree import OIBTree
 from dataFS import DataFS
 import random
+import matplotlib.pyplot as plt
 from multi import MultiProcess
 
 def cul_time(k):
@@ -71,20 +72,19 @@ def train(inputx, inputy, G, C):
     b.append(LR.intercept_)
     return w, b
 
-def predict(K,btree,w,b):
-    res=[]
-    for k in K:
-        if k in btree.keys():
-            res.append(btree[k])
-            continue
-        index = group(k)
-        offset = w[index]*cul_time(k)+b[index]
-        res.append(int(offset[0][0]))
-    return res
+def predict(k,btree,w,b):
+    data = DataFS()
+    if k in btree.keys():
+        v = data.query(k, btree[k])
+        return v
+    index = group(k)
+    offset = w[index]*cul_time(k)+b[index]
+    v = data.query(k,offset)
+    return v
 
 def load_model():
     data = DataFS()
-    keys, _ = data.gen_train_data()
+    keys, label = data.gen_train_data()
     btree = OIBTree()
     rubbish_k = np.load('6k.npy')
     rubbish_v = np.load('6v.npy')
@@ -92,31 +92,39 @@ def load_model():
     b = np.load('b.npy', allow_pickle=True)
     for i in range(len(rubbish_k)):
         btree[str(rubbish_k[i])] = int(rubbish_v[i])
-    return keys, w, b, btree
+    return keys, label,  w, b, btree
 
 def process_run(mp, num, tot, keys, btree, w, b):
     times = int(tot/num)
     for i in range(num):
         mp.add(i, predict, keys[i * times:(i + 1) * times], btree, w, b)
 
-import sys
-print(sys.argv)
-if __name__=='__main__':
-    input,w,b,btree = load_model()
-    keys = input[:int(sys.argv[2])]
-    random.shuffle(keys)
-    time_start = time.time()
-    threads=[]
-    mp = MultiProcess()
-    process_run(mp,int(sys.argv[1]),int(sys.argv[2]),keys,btree,w,b)
-    results=mp.get_res()
-    res=[]
-    print(len(results))
-    for result in results:
-        res.extend(result)
-    time_end = time.time()
-    print(len(res))
-    print('predict:', time_end-time_start,'s')
+# import sys
+# print(sys.argv)
+# if __name__=='__main__':
+#     input,w,b,btree = load_model()
+#     keys = input[:int(sys.argv[2])]
+#     random.shuffle(keys)
+#     time_start = time.time()
+#     threads=[]
+#     mp = MultiProcess()
+#     process_run(mp,int(sys.argv[1]),int(sys.argv[2]),keys,btree,w,b)
+#     results=mp.get_res()
+#     res=[]
+#     print(len(results))
+#     for result in results:
+#         res.extend(result)
+#     time_end = time.time()
+#     print(len(res))
+#     print('predict:', time_end-time_start,'s')
+
+#point search:
+input,label,w,b,btree = load_model()
+keys = input[:100000]
+y = label[:100000]
+for i in keys:
+    print(predict(i,btree,w,b))
+
 
 # with open("btree.pkl","wb") as file:
 #     pickle.dump(btree,file,True)
